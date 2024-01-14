8465cf3d302f52d5992d6749b59460c63a906803
Jostar33 <rhwkd5012@naver.com>
2024-01-14 17:42:19 +0900

    update: query option 설정

--- providers/HydrateProvider.tsx
+++ providers/HydrateProvider.tsx
@@ -13,6 +13,8 @@ interface Props {
   children: React.ReactNode;
 }
 
+const TEN_MINUTES = 10 * 60 * 1000;
+
 export default async function HydrateProvider({
   queryKey,
   queryFn,
@@ -20,10 +22,14 @@ export default async function HydrateProvider({
   children,
 }: Props) {
   const queryClient = getQueryClient();
+  const queryOption = {
+    staleTime: TEN_MINUTES,
+    cacheTime: TEN_MINUTES,
+  };
   // App Route 방식에서는 react-query와 정적 파일 생성을 혼합해서 사용할 경우, 아래와 같이 작성해야함.
   if (isInfiniteQuery)
-    await queryClient.prefetchInfiniteQuery(queryKey, queryFn);
-  else await queryClient.prefetchQuery(queryKey, queryFn);
+    await queryClient.prefetchInfiniteQuery(queryKey, queryFn, queryOption);
+  else await queryClient.prefetchQuery(queryKey, queryFn, queryOption);
   // 하이드레이션으로 감싼후 state에 dehydrate한 후 전달.(건조)
   return <Hydrate state={dehydrate(queryClient)}>{children}</Hydrate>;
 }
