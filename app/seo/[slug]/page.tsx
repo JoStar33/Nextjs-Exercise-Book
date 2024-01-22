b5a8f4d8fc9cce9cd62c12808a69232cdc390bfd
HoCG <rhwkd5012@naver.com>
2024-01-22 22:03:32 +0900

    update: Meta태그 생성에대한 고찰

--- app/seo/[slug]/page.tsx
+++ app/seo/[slug]/page.tsx
@@ -1,8 +1,11 @@
 import { Metadata } from 'next';
+import { Hydrate, dehydrate } from '@tanstack/react-query';
+import { NextSeo } from 'next-seo';
 import { getMovieDetail } from '@/apis/movie';
-import HydrateProvider from '@/providers/HydrateProvider';
 import queryKeys from '@/constants/queryKeys';
 import ReactQueryTestDetailContainer from '@/containers/react-query-test-detail';
+import getQueryClient from '@/utils/getClientQuery';
+import ServerComponent from '@/components/common/ServerComponent';
 
 interface Props {
   params: { slug: string };
@@ -35,15 +38,45 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export default function SeoDetailPage({ params }: Props) {
+const TEN_MINUTES = 10 * 60 * 1000;
+
+// 근데 결론적으로 난 generateMetadata가 있다고 한들 아래 방식을 통해 SEO 최적화를 하는걸 선호할듯하다.
+// 위에 형태로 메타태그를 생성하면 불필요하게 API통신을 두번하는 꼴이 된다. 따라서 서버에 큰 부담을 줄수도 있기에 좋지못하다.
+export default async function SeoDetailPage({ params }: Props) {
   const computedPath = parseInt(params.slug, 10);
+  const queryClient = getQueryClient();
+  const queryOption = {
+    staleTime: TEN_MINUTES,
+    cacheTime: TEN_MINUTES,
+  };
+  const response = await queryClient.fetchQuery(
+    [queryKeys.movieDetail, computedPath],
+    () => getMovieDetail(computedPath),
+    queryOption,
+  );
   return (
-    <HydrateProvider
-      isInfiniteQuery={false}
-      queryFn={() => getMovieDetail(computedPath)}
-      queryKey={[queryKeys.movieDetail, computedPath]}
-    >
-      <ReactQueryTestDetailContainer movieId={computedPath} />
-    </HydrateProvider>
+    <>
+      <NextSeo
+        title={`${response.movie.title}`}
+        description={response.movie.description_full}
+        openGraph={{
+          title: response.movie.title,
+          url: response.movie.url,
+          images: [
+            {
+              url: response.movie.large_cover_image,
+              height: 400,
+              width: 800,
+              alt: '이미지',
+              type: 'image/jpeg',
+            },
+          ],
+        }}
+      />
+      <Hydrate state={dehydrate(queryClient)}>
+        <ServerComponent />
+        <ReactQueryTestDetailContainer movieId={computedPath} />
+      </Hydrate>
+    </>
   );
 }
